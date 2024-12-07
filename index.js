307500280ef209400c875a582befae9cf092c2c6
BTHS_BIGOLDTURKEY <zakariaal2@nycstudents.net>
2024-12-07 11:45:20 -0500

    images are being pulled successfully

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 async function getJSON(){
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos/');
     const json = await response.json();
-    return json;
+    let urls = [];
+    for (let i = 0, len = json.length; i < len; i++) {
+        urls.push(json[i].url);
+    }
+    return urls;
 }
 
 getJSON()
-.then(function (json){
-    for (let i=0; i<3; i++){
-        let data = JSON.stringify(json[i].address);
-        addrs = document.createElement("div");
-        addrs.innerHTML = data; 
-        $("#main").append(addrs);
-    }
-})
+    .then(function (urls){
+        for (let i = 0; i < 5; i++) {
+            let img = document.createElement("img");
+            img.src = urls[i];
+            $("#main").append(img);
+        }
+    })
