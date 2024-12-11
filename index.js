63fbf525890fcd93c07f402434b803fd872f66bc
BTHS_BIGOLDTURKEY <zakariaal2@nycstudents.net>
2024-12-10 19:47:20 -0500

    done

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
+const numPosts = 10;
+
 async function getImages(){
     const response = await fetch('https://jsonplaceholder.typicode.com/photos/');
     const json = await response.json();
-    let urls = [];
-    for (let i = 0, len = json.length; i < len; i++) {
-        urls.push(json[i].url);
-    }
-    return urls;
+    return json.map(img => img.url);
 }
 
 async function getPosts(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/');
-    const json = await response.json();
-    return json;
+    return await response.json();
+}
+
+function displayImages(imgs){
+    imgs.forEach(img => {$("#imgs").append(img)});
+}
+function displayTitles(titles){
+    titles.forEach(title => {$("#titles").append(title)});
+}
+function displayBodies(bodies){
+    bodies.forEach(body => {$("#bodies").append(body)});
 }
 
 let imgs = [];
-function appendImages(imgs){
-    const mainElement = document.getElementById("main");
-    console.log(imgs);
-    console.log("heiuygweifbaiolwf")
-    // imgs.forEach(img => {
-    //     console.log(4)
-    //     mainElement.appendChild(img);
-    // });
-    for (let i=0; i<imgs.length; i++){
-        mainElement.append(imgs[i]);
-    }
-}
-
 getImages()
     .then(urls => {
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < numPosts; i++) {
             let img = document.createElement("img");
-            img.src = urls[i];
+            let index = Math.floor(Math.random()*5000 + 1);
+            img.src = urls[index];
+            img.className = "lesser";
             imgs.push(img);
-            
         }
+        displayImages(imgs);
     });
 
 
@@ -43,16 +39,23 @@ let titles = [];
 let bodies = [];
 getPosts()
     .then(json => {
-        for (let i=0; i<5; i++){
-            let title = json[i].title;
-            titles.push(title);
-            let body = json[i].body;
-            bodies.push(body);
+        for (let i=0; i<numPosts; i++){
+            let index = Math.floor(Math.random()*100 + 1);
+            let title = json[index].title;
+            let titleH4 = document.createElement("h4");
+            titleH4.innerText = title;
+            titleH4.className = "lesser";
+            titles.push(titleH4);
+            let body = json[index].body;
+            let bodyDiv = document.createElement("div");
+            bodyDiv.innerText = body;
+            bodyDiv.className = "lesser";
+            bodies.push(bodyDiv);
         }
+        displayTitles(titles);
+        displayBodies(bodies);
     });
 
 console.log(imgs);
 console.log(titles);
 console.log(bodies);
-
-appendImages(imgs);
